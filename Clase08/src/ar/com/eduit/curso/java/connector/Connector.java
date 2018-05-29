243275e9a71f34e8dd6a9e5d853a97afb919583c
andresdh <andres.delhoyo@gmail.com>
2018-05-29 11:32:43 -0300

    Clase08

---
+++ Clase08/src/ar/com/eduit/curso/java/connector/Connector.java
@@ -0,0 +1,17 @@
+package ar.com.eduit.curso.java.connector;
+
+public class Connector {
+    //driver version 5
+    private static String driver ="com.mysql.jdbc.Driver";
+    //driver version 6 o superior
+    //private static String driver ="com.mysql.cj.jdbc.Driver";
+    
+    private static String vendor="mysql";
+    private static String server="localhost";
+    private static String port="3306";
+    private static String db="colegio";
+    private static String params="";
+    private static String user="root";
+    private static String pass="";
+    
+}
