84b268847dfd07b084a87e5216bccb855ff31f11
andresdh <andres.delhoyo@gmail.com>
2018-05-29 12:56:48 -0300

    Clase08

--- Clase08/src/ar/com/eduit/curso/java/dao/interfaces/I_CursoR.java
+++ Clase08/src/ar/com/eduit/curso/java/dao/interfaces/I_CursoR.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ar.com.eduit.curso.java.dao.interfaces;
 
-/**
- *
- * @author EducaciónIT
- */
+import ar.com.eduit.curso.java.entities.Curso;
+import java.util.List;
+
 public interface I_CursoR {
-    
+    void save(Curso curso);
+    void remove(Curso curso);
+    void update(Curso curso);
+    Curso getById(int id);
+    List<Curso>getAll();
+    List<Curso>getByTitulo(String titulo);
+    List<Curso>getLikeTitulo(String titulo);
+    List<Curso>getByProfesor(String profesor);
+    List<Curso>getLikeProfesor(String profesor);
+    List<Curso>getByDia(String dia);
+    List<Curso>getByTurno(String turno);
 }
